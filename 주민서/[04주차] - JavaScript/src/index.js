1d176de84e2c664b36b38dcc8efeafb209e1142a
주민서 <minseo0526@inu.ac.kr>
2026-03-25 19:49:51 +0900

    조건문

--- 주민서/[04주차] - JavaScript/src/index.js
+++ 주민서/[04주차] - JavaScript/src/index.js
@@ -1,2 +1,17 @@
-let num = 100;
-let result = num % 2 === 0 ? "짝수" : "홀수";
+let fruit = "apple";
+switch (fruit) {
+  case "banana":
+    console.log("바나나");
+    break;
+  case "orange":
+    console.log("오렌지");
+    break;
+  case "apple":
+    console.log("사과");
+    break;
+  case "grape":
+    console.log("포도");
+    break;
+  default:
+    console.log("다른 과일");
+}
