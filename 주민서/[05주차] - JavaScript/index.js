c8c49af8e14bb9fe72cf86f479e6adc1ce8231aa
주민서 <minseo0526@inu.ac.kr>
2026-04-02 22:58:55 +0900

    프로미스 객체

--- 주민서/[05주차] - JavaScript/index.js
+++ 주민서/[05주차] - JavaScript/index.js
@@ -1,3 +1,46 @@
-setTimeout(() => {
-  console.log("비동기");
-}, 3000);
+const workA = (value) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value + 5);
+    }, 5000);
+  });
+  return promise;
+};
+const workB = (value) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value - 3);
+    }, 3000);
+  });
+  return promise;
+};
+const workC = (value) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value + 10);
+    }, 10000);
+  });
+  return promise;
+};
+workA(10)
+  .then((resA) => {
+    console.log(`workA:${resA}`);
+    return workB(resA);
+  })
+  .then((resB) => {
+    console.log(`workB:${resB}`);
+    return workC(resB);
+  })
+  .then((resC) => {
+    console.log(`workC:${resC}`);
+  });
+
+// workA(10, (resA) => {
+//   console.log(`workA:${resA}`);
+//   workB(resA, (resB) => {
+//     console.log(`workB:${resB}`);
+//     workC(resB, (resC) => {
+//       console.log(`workC:${resC}`);
+//     });
+//   });
+// }); 콜백지옥
