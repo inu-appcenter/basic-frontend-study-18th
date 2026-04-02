440f025049118699662cee69e0c4574fae6c59af
주민서 <minseo0526@inu.ac.kr>
2026-04-03 01:05:06 +0900

    API

--- 주민서/[05주차] - JavaScript/index.js
+++ 주민서/[05주차] - JavaScript/index.js
@@ -1,46 +1,5 @@
-const workA = (value) => {
-  const promise = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(value + 5);
-    }, 5000);
-  });
-  return promise;
-};
-const workB = (value) => {
-  const promise = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(value - 3);
-    }, 3000);
-  });
-  return promise;
-};
-const workC = (value) => {
-  const promise = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(value + 10);
-    }, 10000);
-  });
-  return promise;
-};
-workA(10)
-  .then((resA) => {
-    console.log(`workA:${resA}`);
-    return workB(resA);
-  })
-  .then((resB) => {
-    console.log(`workB:${resB}`);
-    return workC(resB);
-  })
-  .then((resC) => {
-    console.log(`workC:${resC}`);
-  });
+let repsonse = fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
 
-// workA(10, (resA) => {
-//   console.log(`workA:${resA}`);
-//   workB(resA, (resB) => {
-//     console.log(`workB:${resB}`);
-//     workC(resB, (resC) => {
-//       console.log(`workC:${resC}`);
-//     });
-//   });
-// }); 콜백지옥
+console.log(repsonse);
