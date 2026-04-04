acbcde83284a48a421dc7e07c1b12f3377361551
주민서 <minseo0526@inu.ac.kr>
2026-04-04 10:32:16 +0900

    생성자 함수 this

--- 주민서/[05주차] - JavaScript/src/index.js
+++ 주민서/[05주차] - JavaScript/src/index.js
@@ -1,9 +1,6 @@
-let $button = document.createElement("button"); //요소 생성
-$button.id = "new-button"; //id 할당
-$button.classList.add("new-button"); //class 할당
-$button.textContent = "버튼"; //텍스트 할당
-$button.addEventListener("click", () => {
-  window.alert("클릭");
-});
-let $animalInfo = document.querySelector("div.animal-info");
-$animalInfo.appendChild($button); //animalInfo의 자식요소에 추가
+function Cafe(menu) {
+  console.log(this);
+  this.menu = menu;
+}
+let myCafe = new Cafe("latte");
+console.log(myCafe);
