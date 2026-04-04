4b6ed5ef4b19c6bf21f059f7da9080e202af8512
주민서 <minseo0526@inu.ac.kr>
2026-04-04 10:55:04 +0900

    동물 앨범

--- 주민서/[05주차] - JavaScript/src/koala.js
+++ 주민서/[05주차] - JavaScript/src/koala.js
@@ -4,8 +4,8 @@ const $content = document.querySelector("div.content");
 let template = "";
 
 //API
-const getData = async () => {
-  let res = await fetch(API_URL);
+const getData = async (name) => {
+  let res = await fetch(`${API_URL}${name}`);
   try {
     if (res) {
       let data = await res.json();
@@ -19,4 +19,4 @@ const getData = async () => {
   }
 };
 
-getData();
+getData("koala");
