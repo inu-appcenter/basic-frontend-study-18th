a469702bbd0bd6910df4b3bf339e208ae4670ef3
jominjoon302 <jominjoon302@gmail.com>
2026-05-10 09:43:46 +0000

    week10

---
+++ 조민준/[10주차] - React/chapter08.js
@@ -0,0 +1,37 @@
+//5가지 요소 순회 및 탐색 메서드
+let arr1 = [1, 2, 3];
+let doubledArr = [];
+
+arr1.forEach((item) => {
+  doubledArr.push(item * 2);
+});
+console.log(doubledArr);
+
+//includes
+//배열에 특정 요소가 있는지 확인하는 메서드
+let arr2 = [1, 2, 3];
+let isIncluded = arr2.includes(3);
+
+console.log(isIncluded);
+
+//indexOf
+//특정 요소의 인덱서(위치)를 찾아서 반환하는 메서드
+let arr3 = [1, 2, 3];
+let index = arr3.indexOf(2);
+console.log(index);
+
+//findIndex
+//모든 요소를 순회하면서, 콜백함수를 만족하는 특정 요소의 인덱스(위치)를 반환하는 메서드
+let arr4 = [1, 2, 3];
+const findedIndex = arr4.findIndex((item1) => {
+  if (item1 % 2 !== 0) return true;
+});
+console.log(findedIndex);
+
+//find
+//모든 요소를 순회하면서 콜백함수를 만족하는 요소를 찾는데, 요소를 그대로 반환
+let arr5 = [{ name: '이정환' }, { name: '홍길동' }];
+
+const finded = arr5.find((item2) => item2.name === '이정환');
+
+console.log(finded);
