4c0860f6f5a5a87f67997afa9d43d1f07987f042
주민서 <minseo0526@inu.ac.kr>
2026-05-17 22:55:19 +0900

    useRef

--- 주민서/[11주차] - React/section04/src/App.jsx
+++ 주민서/[11주차] - React/section04/src/App.jsx
@@ -1,29 +1,11 @@
 import "./App.css";
 import { useState } from "react";
+import Register from "./components/Register";
 
 function App() {
-  const [count, setcount] = useState(0);
-  const [light, setlight] = useState("OFF");
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setlight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          {light === "ON" ? "끄기" : "켜기"}
-        </button>
-      </div>
-      <h1>{count}</h1>
-      <button
-        onClick={() => {
-          setcount(count + 1);
-        }}
-      >
-        +
-      </button>
+      <Register />
     </>
   );
 }
