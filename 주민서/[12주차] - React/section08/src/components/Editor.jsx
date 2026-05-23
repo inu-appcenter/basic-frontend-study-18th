5d47ac52a748535cec618b7270977e121cbaaadc
주민서 <minseo0526@inu.ac.kr>
2026-05-23 09:49:21 +0900

    todolist 기능 구현

--- 주민서/[12주차] - React/section08/src/components/Editor.jsx
+++ 주민서/[12주차] - React/section08/src/components/Editor.jsx
@@ -1,10 +1,40 @@
+import { useRef, useState } from "react";
 import "./Editor.css";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      //엔터 누르면 onSubmit 발동
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      inputRef.current.focus(); //빈 추가 눌럿을때 입력하라고 focus
+      return;
+    }
+    onCreate(content);
+    setContent(""); //추가한뒤 인풋 초기화
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={inputRef}
+        value={content}
+        onChange={onChangeContent}
+        onKeyDown={onKeydown}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
