34f2af1860d8ee10216b6b349e8d167111f91292
주민서 <minseo0526@inu.ac.kr>
2026-05-23 09:17:25 +0900

    기능구현 준비하기

---
+++ 주민서/[12주차] - React/section08/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import { useState } from "react";
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "리액트 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래연습하기",
+    date: new Date().getTime(),
+  },
+];
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  return (
+    <div className="App">
+      <>
+        <Header />
+        <Editor />
+        <List />
+      </>
+    </div>
+  );
+}
+
+export default App;
