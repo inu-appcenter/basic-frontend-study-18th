27a41759dd569167942d430325dfe65c18385bdb
주민서 <minseo0526@inu.ac.kr>
2026-05-29 12:10:35 +0900

    useReducer

--- 주민서/[12주차] - React/section08/src/App.jsx
+++ 주민서/[12주차] - React/section08/src/App.jsx
@@ -3,7 +3,7 @@ import { useRef, useState } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
-
+import Exam from "./components/ex";
 const mockData = [
   {
     id: 0,
@@ -59,9 +59,10 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Exam />
+      {/* <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
     </div>
   );
 }
