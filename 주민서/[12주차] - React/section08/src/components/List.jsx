53efdf9bf60ddf038b19cc03e39bbc6d9ac7fce7
주민서 <minseo0526@inu.ac.kr>
2026-05-29 12:46:06 +0900

    getFilteredData 함수

--- 주민서/[12주차] - React/section08/src/components/List.jsx
+++ 주민서/[12주차] - React/section08/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -20,9 +20,28 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalyzedData 호출!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+  // 의존성배열 : deps
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
